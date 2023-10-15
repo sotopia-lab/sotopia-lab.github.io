a089c826efe288a937a666476416b9be4e868eca
XuhuiZhou <zhouxuhui2018@gmail.com>
2023-10-15 15:35:33 -0400

    🐛 responsive diagram

--- app/evaluation.tsx
+++ app/evaluation.tsx
@@ -2,7 +2,7 @@ import { rewardDiagram } from "@/components/rewards";
 
 export const Evaluation = () => {
     return (
-        <div className="lg:col-span-6">
+        <div className="lg:col-span-6 col-span-12">
             <div className="rounded-2xl pb-4 dark:bg-black dark:text-white">
                 <h1 className="text-center font-sans text-xl">Scores for Agent2</h1>
                 <p className="text-center font-sans text-sm italic">Role-played character: Noah Davis</p>
