40b14950062312006886ce16ab9894fb3dab81c2
Hao Zhu <prokilchu@gmail.com>
2023-10-16 19:24:33 -0400

    ✨ some of the avatars

--- components/ui/model-toggle.tsx
+++ components/ui/model-toggle.tsx
@@ -5,19 +5,14 @@ import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
  
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+
  
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
   const [_, startTransition] = React.useTransition();    
   return (
     <Button
-      variant="outline"
+      variant="ghost"
       size="icon"
       onClick={() => {
         startTransition(() => {
@@ -25,12 +20,10 @@ export function ModeToggle() {
         });
     }}
     >
-      {!theme ? null : theme === 'dark' ? (
         <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 transition-all" />
-          ) : (
         <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 transition-all" />
-        )}
         <span className="sr-only">Toggle theme</span>
       </Button>
+      
   )
 }
