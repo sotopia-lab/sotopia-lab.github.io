e0b406b7546addcdab62955fdb62467e419e391d
XuhuiZhou <zhouxuhui2018@gmail.com>
2024-09-11 16:15:09 -0400

    remove the server side of things

--- next.config.js
+++ next.config.js
@@ -1,33 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  output: 'export',
   images: {
+    unoptimized: true,
+    reactStrictMode: true,
     domains: [
       "xd2kcvzsdpeyx1gu.public.blob.vercel-storage.com",
       "replicate.delivery",
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: "/github",
-        destination: "https://github.com/steven-tey/sotopia",
-        permanent: false,
-      },
-      {
-        source: "/deploy",
-        destination: "https://vercel.com/templates/next.js/sotopia",
-        permanent: false,
-      },
-      {
-        source: "/t",
-        destination: "/",
-        permanent: false,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/github",
+  //       destination: "https://github.com/steven-tey/sotopia",
+  //       permanent: false,
+  //     },
+  //     {
+  //       source: "/deploy",
+  //       destination: "https://vercel.com/templates/next.js/sotopia",
+  //       permanent: false,
+  //     },
+  //     {
+  //       source: "/t",
+  //       destination: "/",
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
